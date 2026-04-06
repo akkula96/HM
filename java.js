078a9d39952a2609d1e5ae8b8774b94501d209e5
Orman <akkula96@gmail.com>
2026-04-06 22:43:28 +0600

    исправленный

--- java.js
+++ java.js
@@ -1,51 +1,57 @@
+const title = document.getElementById('title');
+const redLight = document.getElementById('red');
+const yellowLight = document.getElementById('yellow');
+const greenLight = document.getElementById('green')
+const msg = document.getElementById('msg')
+
+
 function changeBg() {
     let color = prompt("Введите цвет: red, blue, green, yellow, purple");
     
     if (color === "red") {
         document.body.style.background = "red";
-        document.getElementById("title").innerText = "Фон красный";
+        title.innerText = "Фон красный";
     }
     else if (color === "blue") {
         document.body.style.background = "blue";
-        document.getElementById("title").innerText = "Фон синий";
+        title.innerText = "Фон синий";
     }
     else if (color === "green") {
         document.body.style.background = "green";
-        document.getElementById("title").innerText = "Фон зелёный";
+        title.innerText = "Фон зелёный";
     }
     else if (color === "yellow") {
         document.body.style.background = "yellow";
-        document.getElementById("title").innerText = "Фон жёлтый";
+        title.innerText = "Фон жёлтый";
     }
     else if (color === "purple") {
         document.body.style.background = "purple";
-        document.getElementById("title").innerText = "Фон фиолетовый";
+        title.innerText = "Фон фиолетовый";
     }
     else {
         alert("Ошибка! Введите red, blue, green, yellow, purple");
     }
 }
-
 function trafficLight() {
-    let color = prompt("Введите цвет: Красный, Жёлтый или Зелёный");
+    let color = prompt("Введите цвет: red, yellow или green");
     
-    document.getElementById("red").classList.remove("active");
-    document.getElementById("yellow").classList.remove("active");
-    document.getElementById("green").classList.remove("active");
-    
-    if (color === "Красный") {
-        document.getElementById("red").classList.add("active");
-        document.getElementById("msg").innerText = "STOP";
+    redLight.classList.remove("active");
+    yellowLight.classList.remove("active");
+    greenLight.classList.remove("active");
+
+    if (color === "red") {
+        redLight.classList.add("active");
+        msg.innerText = "STOP";
     }
-    else if (color === "Жёлтый") {
-        document.getElementById("yellow").classList.add("active");
-        document.getElementById("msg").innerText = "WAIT";
+    else if (color === "yellow") {
+        yellowLight.classList.add("active");
+        msg.innerText = "WAIT";
     }
-    else if (color === "Зелёный") {
-        document.getElementById("green").classList.add("active");
-        document.getElementById("msg").innerText = "GO";
+    else if (color === "green") {
+       greenLight.classList.add("active");
+       msg.innerText = "GO";
     }
     else {
-        alert("Ошибка! Введите: Красный, Жёлтый или Зелёный");
+        alert("Ошибка! Введите: red, yellow или green");
     }
 }
